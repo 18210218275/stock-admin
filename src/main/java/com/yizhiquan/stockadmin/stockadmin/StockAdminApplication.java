970369cb1be3e2b38e0df443b91e3d4dafb4ba26
luqinshun-ds <luqinshun@gome.com.cn>
2019-06-12 19:48:17 +0800

    mybatis配置修改

--- src/main/java/com/yizhiquan/stockadmin/stockadmin/StockAdminApplication.java
+++ src/main/java/com/yizhiquan/stockadmin/stockadmin/StockAdminApplication.java
@@ -1,11 +1,13 @@
 package com.yizhiquan.stockadmin.stockadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@MapperScan("com.yizhiquan.stockadmin.stockadmin.dao")
 public class StockAdminApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/yizhiquan/stockadmin/stockadmin/config/WebMvcConfig.java
+++ src/main/java/com/yizhiquan/stockadmin/stockadmin/config/WebMvcConfig.java
@@ -16,6 +16,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+
     }
 
 }
